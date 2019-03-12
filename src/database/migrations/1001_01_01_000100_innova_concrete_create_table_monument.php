6548a3275a8dc5de1e9e7a80509f8c4b30647a8f
Carlos Rodríguez Palacín <cpalacin@digitalh2.com>
2019-03-12 23:32:34 +0100

    set database

--- src/database/migrations/1001_01_01_000100_innova_concrete_create_table_monument.php
+++ src/database/migrations/1001_01_01_000100_innova_concrete_create_table_monument.php
@@ -18,28 +18,28 @@ class InnovaConcreteCreateTableMonument extends Migration
 				$table->engine = 'InnoDB';
 
                 $table->increments('id');
-				$table->string('original_name');
+				$table->string('original_name')->nullable();
                 $table->string('current_name');
-                $table->string('other_denominations');
-                $table->string('original_use');
-                $table->string('current_use');
-                $table->integer('commission')->unsigned();
-                $table->integer('completion')->unsigned();
-                $table->text('description');
-                $table->string('rapporteur_name');
-                $table->string('rapporteur_email');
-                $table->integer('rapporteur_date')->unsigned();
-
-                $table->json('links'); // object [{name:'',url:''}]
+                $table->string('other_denominations')->nullable();
+                $table->string('original_use')->nullable();
+                $table->string('current_use')->nullable();
+                $table->integer('commission')->unsigned()->nullable();
+                $table->integer('completion')->unsigned()->nullable();
+                $table->text('description')->nullable();
+                $table->string('rapporteur_name')->nullable();
+                $table->string('rapporteur_email')->nullable();
+                $table->integer('rapporteur_date')->unsigned()->nullable();
+                $table->tinyInteger('percentage')->unsigned()->nullable();
+                $table->json('links')->nullable(); // object [{name:'',url:''}]
 
                 // geolocation data
-                $table->string('country_id', 2)->nullable();
+                $table->string('country_id', 2);
                 $table->string('province')->nullable();
                 $table->string('address')->nullable();
-                $table->string('locality')->nullable();
+                $table->string('locality');
                 $table->string('zip')->nullable();
-                $table->decimal('latitude', 17, 14)->nullable();
-                $table->decimal('longitude', 17, 14)->nullable();
+                $table->decimal('latitude', 17, 14);
+                $table->decimal('longitude', 17, 14);
 
                 $table->timestamps();
                 $table->softDeletes();
