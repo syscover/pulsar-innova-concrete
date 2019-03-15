8525be57e6661ae045abfa7cf248129b8c42f4ac
Carlos Rodríguez Palacín <cpalacin@digitalh2.com>
2019-03-15 20:06:51 +0100

    fixed type ID

--- src/Techedge/InnovaConcrete/Models/People.php
+++ src/Techedge/InnovaConcrete/Models/People.php
@@ -13,6 +13,13 @@ class People extends CoreModel
     protected $fillable     = ['name', 'group_id'];
     public $with            = ['group'];
 
+    public function scopeBuilder($query)
+    {
+        return $query
+            ->join('innova_concrete_group', 'innova_concrete_people.group_id', '=', 'innova_concrete_group.id')
+            ->addSelect('innova_concrete_group.*', 'innova_concrete_people.*', 'innova_concrete_group.name as innova_concrete_group_name', 'innova_concrete_people.name as innova_concrete_people_name');
+    }
+
     public function group()
     {
         return $this->belongsTo(Group::class, 'group_id');
