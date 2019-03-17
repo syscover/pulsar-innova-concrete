b6a51e7299cb752b0bc19c8eb40c9b742dfc8e7f
Carlos Rodríguez Palacín <cpalacin@digitalh2.com>
2019-03-17 21:06:05 +0100

    add resources

--- src/database/seeds/InnovaConcreteResourceTableSeeder.php
+++ src/database/seeds/InnovaConcreteResourceTableSeeder.php
@@ -8,7 +8,13 @@ class InnovaConcreteResourceTableSeeder extends Seeder {
     public function run()
     {
         Resource::insert([
-            ['id' => 'innova-concrete',               'name' => 'Innova Concrete',                              'package_id' => 1001],
+            ['id' => 'innova',                          'name' => 'Innova Concrete Package',                    'package_id' => 1001],
+            ['id' => 'innova-monument',                 'name' => 'Monument',                                   'package_id' => 1001],
+            ['id' => 'innova-database',                 'name' => 'Database',                                   'package_id' => 1001],
+            ['id' => 'innova-characteristic',           'name' => 'Characteristic',                             'package_id' => 1001],
+            ['id' => 'innova-type',                     'name' => 'Type',                                       'package_id' => 1001],
+            ['id' => 'innova-people',                   'name' => 'People',                                     'package_id' => 1001],
+            ['id' => 'innova-group',                    'name' => 'Group',                                      'package_id' => 1001],
         ]);
     }
 }
